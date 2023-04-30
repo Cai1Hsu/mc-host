ee40de0f86377d936d3233f4f34628cd39ac0ce3
Caiyi Hsu <cai1hsu@outlook.com>
2023-04-30 11:15:41 +0000

    Clean debug code

--- Program.cs
+++ Program.cs
@@ -1,26 +1,8 @@
-using mchost.Server;
 using mchost.Logging;
 
 const bool TECHNICAL_PREVIEW = true;
 
 Logger.Log("Welcome to mchost!");
-
-var host = new ServerHost();
-
-ServerHost.SetServerHost(host);
-
-ServerHost.GetServerHost()?.StartServer("/usr/lib/jvm/java-17-openjdk/bin/java", "-server -Xmx1024M -Xms1024M -jar server.jar nogui");
-
-bool quit = false;
-
-Console.CancelKeyPress += (sender, e) =>
-{
-    e.Cancel = true;
-    quit = true;
-};
-
-SpinWait.SpinUntil(() => quit);
-return;
 
 var builder = WebApplication.CreateBuilder(args);
 
